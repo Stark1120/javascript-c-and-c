df4e1b075d7c2eadd0e2de8343843def730d3548
Stark1120 <152051955+Stark1120@users.noreply.github.com>
2025-05-15 10:16:44 +0000

    this is the datatype summary

---
+++ javascript/DataTypeSummary.js
@@ -0,0 +1,28 @@
+// primitive datatypes
+// 7 types: string, Number, boolean, null, undefined, symbol BigInt
+
+
+//Reference DataTypes(NonPrimitveDataType):
+
+// arrays, Objects, Functions
+
+const id=Symbol("123")
+const anotherid=Symbol("123")
+// if(id===anotherid)
+// {
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//null ka datatype hmesha object hota hai
+// const hero=['ldavd','lasam']
+// console.log(hero[1])
+
+
+// const myfunction=function(){
+//     console.log("hellow")
+// }
+// console.log(typeof myfunction)
+console.log(typeof anotherid)
