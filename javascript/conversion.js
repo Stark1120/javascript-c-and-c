f8fce9ef179e148cef76ef258fc993cadca851ee
Stark1120 <152051955+Stark1120@users.noreply.github.com>
2025-05-15 09:04:28 +0000

    conversion from one datatype to another datatype

---
+++ javascript/conversion.js
@@ -0,0 +1,13 @@
+let rollno="123 abc"
+console.log(typeof rollno)
+let string=String(rollno)
+console.log(typeof string)
+console.log(typeof rollno)
+let newrollno=Number(rollno)
+console.log(newrollno)
+let isLoggedIn=true
+let islogin=String(isLoggedIn)
+console.log(islogin)
+let age="23"
+let newage=String(age)
+console.log(newage)
