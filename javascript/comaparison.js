a3c3366885cb367ae4249121e3ca5e3f5fbdd587
Stark1120 <152051955+Stark1120@users.noreply.github.com>
2025-05-15 09:52:31 +0000

    this is the comparion operation performinng file

---
+++ javascript/comaparison.js
@@ -0,0 +1,13 @@
+// console.log("2">11)
+// console.log(null>0)
+// console.log(0<null)
+// console.log(0=false
+// )
+// console.log(null>=0)
+// console.log("1"+2+3 ) output:123
+// console.log(2+4+5+"56") output:1156/
+
+// while double equal to(==) cheack only the comaparion between 2 varialable where the (===) it check the  data type of the variable
+
+console.log("2="==2)//it converted by the system string datatype to the interger for the comparison
+console.log("2"===2) //it cheack the variable value and also there datatype in the case of this varaibles values are the same but datatype are different therefore the output gives u fals
