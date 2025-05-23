6166d5c3d911f5a9741a1d23814e99fda13f994a
Stark1120 <152051955+Stark1120@users.noreply.github.com>
2025-05-23 10:14:45 +0000

    String operations

---
+++ javascript/String.js
@@ -0,0 +1,12 @@
+const name="govinda"
+const age=30
+console.log(`my name is ${name} and age is  ${age}`)
+const lname=new String("govinda prajapati")
+console.log(lname.length)
+console.log(lname.indexOf("t"))
+console.log(lname.charAt(5))
+const newname=lname.substring(0,4)
+console.log(newname)
+const slice=lname.slice(0,7)
+console.log(slice)
+console.log(lname.concat( " lavda lasan"))
